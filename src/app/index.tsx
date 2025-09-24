8095e98b632f49cc2d067abae30941892bea92e9
juniormartinez1020 <juniormperalta33@gmail.com>
2025-09-24 19:48:00 -0400

    configuracion supabase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,17 @@
 import { View } from "react-native";
 import Entypo from '@expo/vector-icons/Entypo';
 import { Link } from "expo-router";
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
 
 export default function Home() {
+
+  useEffect(() => {
+    supabase.from('events').select('*, assets(*)')
+    .then((data) => console.log(JSON.stringify(data, null, 2)))
+  }, [])
+  
+
   return (
       <View
       className="flex-1 justify-center items-center
